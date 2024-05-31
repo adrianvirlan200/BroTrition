848e29a48e9561ad55de3f02043c9bdcbedc79ce
adrian.virlan200 <adrian.virlan2903@stud.acs.upb.ro>
2024-05-31 13:42:49 +0300

    Added 2 independent columns in the main page

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -13,6 +13,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [updateSignal, setUpdateSignal] = useState(false);
+
   const handleUpdateTable = () => {
     setUpdateSignal((prevData) => !prevData);
   };
@@ -27,30 +28,34 @@ export default function Home() {
 
   return (
     <div className="ml-60 mt-6 w-full">
-      <div className="ml-6 grid lg:grid-cols-[3fr_1fr] sm:grid-cols-1 gap-5">
-        <div className="border-1 border-gray-300 w-full p-2 bg-white rounded-2xl">
-          <div className="m-2">
-            <AddFoodButton onUpdate={handleUpdateTable} />
-            <AddExerciseButton onUpdate={handleUpdateTable} />
-            <AddBiometricsButton onUpdate={handleUpdateTable} />
-            <AddNoteButton onUpdate={handleUpdateTable} />
+      <div className="ml-6 grid lg:grid-cols-[3fr_1fr] sm:grid-cols-1 gap-3">
+        {/* Left column */}
+        <div>
+          <div className="border-1 border-gray-300 w-full p-2 bg-white rounded-2xl">
+            <div className="m-2">
+              <AddFoodButton onUpdate={handleUpdateTable} />
+              <AddExerciseButton onUpdate={handleUpdateTable} />
+              <AddBiometricsButton onUpdate={handleUpdateTable} />
+              <AddNoteButton onUpdate={handleUpdateTable} />
+            </div>
+            <MainTable
+              currentDate={date}
+              onDelete={handleUpdateTable}
+              updateSignal={updateSignal}
+            />
           </div>
-          <MainTable
-            currentDate={date}
-            onDelete={handleUpdateTable}
-            updateSignal={updateSignal}
-          />
+
+          <div className="w-full mt-3 border-1 border-gray-300 content-center p-2 bg-white rounded-2xl">
+            <MacroStats currentDate={date} updateSignal={updateSignal} />
+          </div>
         </div>
 
-        <div className="">
+        {/* Right column */}
+        <div>
           <div className="border-1 border-gray-300 content-center w-fit p-2 bg-white rounded-2xl">
             <MainCalendar setDate={handleSetDate} />
             <CalHistory updateSignal={updateSignal} />
           </div>
-        </div>
-
-        <div className="w-full border-1 border-gray-300 content-center p-2 bg-white rounded-2xl">
-          <MacroStats currentDate={date} updateSignal={updateSignal} />
         </div>
       </div>
     </div>
