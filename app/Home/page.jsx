0bf0f040e026a97537ab7adcb90f817b9a061164
adrian.virlan200 <adrian.virlan2903@stud.acs.upb.ro>
2024-06-02 22:12:17 +0300

    added shadow to the cards in the home page

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -31,7 +31,7 @@ export default function Home() {
     <div className="grid lg:grid-cols-[3fr_1fr] grid-cols-1 md:grid-cols-1 gap-4">
       {/* Left column */}
       <div>
-        <div className="border-1 border-gray-300 w-full p-2 bg-white rounded-2xl">
+        <div className="border-1 border-gray-300 w-full p-2 bg-white rounded-2xl shadow-md">
           <div className="m-2 grid grid-cols-2 lg:grid-cols-4">
             <AddFoodButton onUpdate={handleUpdateTable} className="mx-auto" />
             <AddExerciseButton
@@ -51,11 +51,11 @@ export default function Home() {
           />
         </div>
 
-        <div className="w-full mt-3 border-1 border-gray-300 content-center p-2 bg-white rounded-2xl">
+        <div className="w-full mt-3 border-1 border-gray-300 content-center p-2 bg-white rounded-2xl shadow-md">
           <MacroStats currentDate={date} updateSignal={updateSignal} />
         </div>
 
-        <div className="w-full mt-3 border-1 border-gray-300 content-center p-2 bg-white rounded-2xl">
+        <div className="w-full mt-3 border-1 border-gray-300 content-center p-2 bg-white rounded-2xl shadow-md">
           <div className="h-screen">
             ToDo: add progress bars for all micro-nutrients
           </div>
@@ -64,11 +64,11 @@ export default function Home() {
 
       {/* Right column */}
       <div>
-        <div className="border-1 border-gray-300 content-center w-fit p-2 bg-white rounded-2xl">
+        <div className="border-1 border-gray-300 content-center w-fit p-2 bg-white rounded-2xl shadow-md">
           <MainCalendar setDate={handleSetDate} />
           <CalHistory updateSignal={updateSignal} />
         </div>
-        <div className="mt-4 mx-auto p-2 w-full border-1 border-gray-300 content-center py-2 bg-white rounded-2xl">
+        <div className="mt-4 mx-auto p-2 w-full border-1 border-gray-300 content-center py-2 bg-white rounded-2xl shadow-md">
           <TryGold />
         </div>
       </div>
